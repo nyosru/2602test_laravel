69f9735d558435165200de11329b5c572f6293ec
Сергей <1@php-cat.com>
2026-02-06 00:15:08 +0500

    ++ cicd 11

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,7 +1,5 @@
 <?php
 
-use Laravel\Sanctum\Sanctum;
-
 return [
 
     /*
@@ -80,7 +78,7 @@ return [
 //        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
 //        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
 //        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'verify_csrf_token' => \App\Http\Middleware\VerifyCsrfToken::class,
 //        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
     ],
 
