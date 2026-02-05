f3da63dc6ce9662c6d48d7ae6ba66c44ff85ec62
Сергей <1@php-cat.com>
2026-02-06 02:05:08 +0500

    ++ cicd 22

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-db', function () {
+    return [
+        'connection' => config('database.default'),
+        'database' => config('database.connections.' . config('database.default') . '.database'),
+        'file_exists' => file_exists(config('database.connections.sqlite.database')),
+    ];
+});
