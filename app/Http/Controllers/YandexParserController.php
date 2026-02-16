e9cc0f183bbffa9ad7ae41d5ff1bba091a982771
nyosru <nyos@rambler.ru>
2026-02-16 05:46:21 +0500

    ++ парсинг янд22

--- app/Http/Controllers/YandexParserController.php
+++ app/Http/Controllers/YandexParserController.php
@@ -180,6 +180,8 @@ class YandexParserController extends Controller
                         '--disable-extensions',
                         '--disable-setuid-sandbox',
                         '--remote-debugging-port=9222',       // явно указываем порт
+                        '--no-zygote',                    // ← добавь это
+                        '--single-process',
                         '--disable-background-networking',
                         '--window-size=1920,1080',
                         '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
