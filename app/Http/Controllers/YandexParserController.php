d9853871759da3dcde139290ea0329fd8ac7b834
nyosru <nyos@rambler.ru>
2026-02-16 05:31:30 +0500

    ++ парсинг яндекса1123

--- app/Http/Controllers/YandexParserController.php
+++ app/Http/Controllers/YandexParserController.php
@@ -140,7 +140,7 @@ class YandexParserController extends Controller
         }
     }
 
-    public function get_from_panther(string $url , bool $refresh = false )
+    public function get_from_panther(string $url, bool $refresh = false)
     {
 
         if (!$url || !str_contains($url, 'yandex.ru/maps')) {
@@ -172,21 +172,21 @@ class YandexParserController extends Controller
                 $client = PantherClient::createChromeClient(
                     '/usr/lib/chromium-browser/chromedriver',
                     [
-                    '--headless=new',                     // новый headless-режим (более стабильный)
-                    '--no-sandbox',
-                    '--disable-dev-shm-usage',
-                    '--disable-gpu',
-                    '--disable-software-rasterizer',
-                    '--disable-extensions',
-                    '--disable-setuid-sandbox',
-                    '--remote-debugging-port=9222',       // явно указываем порт
-                    '--disable-background-networking',
-                    '--window-size=1920,1080',
-                    '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                ],
+                        '--headless=new',                     // новый headless-режим (более стабильный)
+                        '--no-sandbox',
+                        '--disable-dev-shm-usage',
+                        '--disable-gpu',
+                        '--disable-software-rasterizer',
+                        '--disable-extensions',
+                        '--disable-setuid-sandbox',
+                        '--remote-debugging-port=9222',       // явно указываем порт
+                        '--disable-background-networking',
+                        '--window-size=1920,1080',
+                        '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                    ],
                     [
                         'chrome' => [
-                            'binary' => '/usr/bin/chromium-browser',  // или /usr/lib/chromium-browser/chrome
+                            'binary' => '/usr/bin/chromium',  // или /usr/lib/chromium-browser/chrome
                         ],
                     ]);
 
@@ -275,16 +275,14 @@ class YandexParserController extends Controller
 
                 // Если хочешь убрать "из 5" или лишнее
                 $rating = preg_replace('/\s*из\s*\d+.*$/i', '', $rating);
-                $rating = str_replace( 'Rating ','', $rating );
+                $rating = str_replace('Rating ', '', $rating);
             }
-
 
 
             $businesAspects = $crawler->filter('.business-aspect-view')->each(function (Crawler $node) {
                 // Чистый текст без лишних пробелов
                 return preg_replace('/\s+/', ' ', trim($node->text()));
             });
-
 
 
             return response()->json([
