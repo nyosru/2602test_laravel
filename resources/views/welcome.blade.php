e88e8822fb3b3aed9a84759da41f50180b474122
nyosru <nyos@rambler.ru>
2026-02-23 21:50:27 +0500

    ++ +счетчик

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -183,11 +183,13 @@
                     3. Как вы изолируете staging от production.
                 </p>
 
-                <p class="font-bold text-gray-900">Ответ:</p>
             </div>
 
             <!-- Видео (справа) -->
             <div class="w-full lg:w-2/5 lg:sticky lg:top-8">
+
+                <p class="font-bold text-gray-900">Ответ:</p>
+
 {{--                <div class="aspect-w-16 aspect-h-12 shadow-xl rounded-lg overflow-hidden">--}}
                     <iframe
                         src="https://vkvideo.ru/video_ext.php?oid=-73827323&id=456239023&hash=198d61ff7b00ab6c&hd=1"
@@ -208,5 +210,20 @@
 <br/>
 <br/>
 <br/>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106970560', 'ym');
+
+    ym(106970560, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/106970560" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
